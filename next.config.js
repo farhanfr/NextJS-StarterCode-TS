46c158133d45b7d87316c68887b5a18c0ad1577f
Mochamad Farhan Fitrahtur Rachmad <farhanfitrahtur@gmail.com>
2024-06-24 10:11:56 +0700

    fixing bug and allow to ignoreBuildError

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    productionBrowserSourceMaps:false,
     env:{
         BASE_URL:process.env.BASE_URL,
         API_KEY:process.env.API_KEY
-    }
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+      },
 }
 
 module.exports = nextConfig
