7f9f9b2f4775202d91424d9342bdc72a676135d7
Mochamad Farhan Fitrahtur Rachmad <farhanfitrahtur@gmail.com>
2024-06-24 10:58:40 +0700

    configure middleware

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import { withAuth } from "next-auth/middleware";
 import { NextRequest, NextResponse } from "next/server";
 
 
-const adminPath = {
-    protectedUrl: ["/dashboard/:path*"],
-}
+// const urlGeneral = {
+//     protectedUrl: ["/dashboard/:path*"],
+// }
 
 export default withAuth(
     async function middleware(req) {
@@ -16,9 +16,19 @@ export default withAuth(
         console.log("isAuthenticated => ", isAuthenticated)
         if (isAuthenticated) {
             const user = token.user
+            console.log("URL AUTH=> ", req.nextUrl.pathname)
+            if (req.nextUrl.pathname.startsWith('/auth/login')) {
+                return NextResponse.redirect(new URL('/dashboard?callbackUrl=' + encodeURIComponent(req.url), req.url))
+            }
+            if (req.nextUrl.pathname.startsWith('/auth/register')) {
+                return NextResponse.redirect(new URL('/dashboard?callbackUrl=' + encodeURIComponent(req.url), req.url))
+            }
+            if (req.nextUrl.pathname.startsWith('/auth/otp')) {
+                return NextResponse.redirect(new URL('/dashboard?callbackUrl=' + encodeURIComponent(req.url), req.url))
+            }
         } else {
             if (req.nextUrl.pathname.startsWith('/dashboard')) {
-                return NextResponse.redirect(new URL('/auth/login'))
+                return NextResponse.redirect(new URL('/auth/login?callbackUrl=' + encodeURIComponent(req.url), req.url))
             }
             return NextResponse.next()
         }
